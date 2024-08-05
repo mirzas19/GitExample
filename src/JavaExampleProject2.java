b71210c7b42f73592daf0a83b8e4f721295d58c1
mirzas19 <mirzasultanmoinuddin19@gmail.com>
2024-08-05 08:44:54 -0500

    FirstCommit

---
+++ src/JavaExampleProject2.java
@@ -0,0 +1,8 @@
+package com.example.project2;
+        public class JavaExampleProject2  {
+                public static void main(String[] args) {
+                        System.out.println("Hello World");
+
+                }
+
+        }
