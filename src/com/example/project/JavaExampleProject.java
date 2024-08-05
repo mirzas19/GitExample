1f71a1f9b1c34d697008da598ead8d21851edad4
mirzas19 <mirzasultanmoinuddin19@gmail.com>
2024-08-05 08:44:20 -0500

    FirstCommit

---
+++ src/com/example/project/JavaExampleProject.java
@@ -0,0 +1,17 @@
+package com.example.project;
+
+public class JavaExampleProject {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        //byte, int, short, long
+        byte b = 120;
+        System.out.println(b);
+        int a = 137;
+        System.out.println(a);
+        short s = 127;
+        System.out.println(s);
+        long l = 1278787;
+        System.out.println(l);
+
+    }
+}
